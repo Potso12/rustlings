4bc92139e7c509479dfeb2799fb47863642da9fd
Otso Tikkanen <otso.tikkanen@tuni.fi>
2024-12-19 21:14:43 +0200

    add missing parts

--- exercises/23_conversions/from_str.rs
+++ exercises/23_conversions/from_str.rs
@@ -41,7 +41,22 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
 
-    fn from_str(s: &str) -> Result<Self, Self::Err> {}
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let parts = s.split(",");
+
+        let collection = parts.collect::<Vec<&str>>();
+
+        if collection.len() != 2 {
+            return Err(ParsePersonError::BadLen)
+        } else if collection[0].is_empty() {
+            return Err(ParsePersonError::NoName)
+        } 
+         
+        match collection[1].parse::<u8>() {
+            Ok(age) => Ok(Person {name: collection[0].to_string(), age}),
+            Err(e) => Err(ParsePersonError::ParseInt(e))
+        }
+    }
 }
 
 fn main() {
